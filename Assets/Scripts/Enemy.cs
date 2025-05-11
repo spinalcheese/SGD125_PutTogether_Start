b512163a95760366a094d613b5741517c45efd05
cheez <alexedmayo@gmail.com>
2025-05-11 14:18:58 -0400

    added player tracking and patrol AI

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    Transform target;
+    NavMeshAgent agent;
+    int currentNode;
+    int previousNode;
+    public enum EnemyState
+    {
+        patrol,
+        chase
+    };
+    EnemyState enemyState = EnemyState.patrol;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        currentNode = Random.Range(0, GameManager.gm.nodes.Length);
+        previousNode = currentNode;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (enemyState)
+        {
+            case EnemyState.patrol: Patrol(); break;
+            case EnemyState.chase: Chase(); break;
+            default: break;
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "node")
+        {
+            currentNode = Random.Range(0, GameManager.gm.nodes.Length);
+            while(currentNode == previousNode)
+            {
+                currentNode = Random.Range(0, GameManager.gm.nodes.Length);
+            }
+            previousNode = currentNode;
+        }
+
+        if(other.tag == "Player")
+        {
+            enemyState = EnemyState.chase;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            enemyState = EnemyState.patrol;
+        }
+    }
+
+    void Patrol()
+    {
+        agent.destination = GameManager.gm.nodes[currentNode].position;
+    }
+    void Chase()
+    {
+        agent.destination = GameManager.gm.player.transform.position;
+    }
+
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    #region
+    public static GameManager gm;
+
+    void Awake()
+    {
+        if (!gm)
+            gm = this;
+        else
+            Destroy(this);
+    }
+    #endregion
+    public GameObject player;
+    public Transform[] nodes;
+
+}
